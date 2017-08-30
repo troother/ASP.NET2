ce76dd20f5c5325887144506b8907c8566ff0ca2
Troother <Troother@DESKTOP-BRBQ07J>
2017-08-30 14:19:20 +0200

    shoppingcart with function add has been created

--- OnlinePizzeria/Controllers/CartController.cs
+++ OnlinePizzeria/Controllers/CartController.cs
@@ -4,21 +4,85 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OnlinePizzeria.Models;
+using OnlinePizzeria.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace OnlinePizzeria.Controllers
 {
     public class CartController : Controller
     {
-        // GET: Cart
-        public ActionResult Index()
+
+        private readonly ApplicationDbContext _context;
+
+        public CartController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        // GET: Cart
+        public async Task<ActionResult> Index()
+        {
+            List<CartItem> cartItems = new List<CartItem>();
+
+            if (HttpContext.Session.GetInt32("Cart") == null)
+            {
+                var carts = await _context.Carts.ToListAsync();
+                var newId = HttpContext.Session.GetInt32("Cart");
+                newId = carts.Count + 1;
+
+                cartItems = new List<CartItem>();
+            }
+
+            else
+            {
+                Cart cart = new Cart();
+                var cartId = HttpContext.Session.GetInt32("Cart");
+                cart = await _context.Carts.Include(x => x.Items).ThenInclude(z => z.Dish).SingleOrDefaultAsync(y => y.CartId == cartId);
+                cartItems = cart.Items;
+            }
+            return View(cartItems);
         }
 
         // GET: Cart/Details/5
-        public ActionResult Details(int id)
+        public async Task<ActionResult> AddToCart(int id)
         {
-            return View();
+            Dish dish = _context.Dishes.FirstOrDefault(x => x.Id == id);
+
+            if (HttpContext.Session.GetInt32("Cart") == null)
+            {
+                Cart cart = new Cart();
+                List<CartItem> cartItems = new List<CartItem>();
+                var carts = await _context.Carts.ToListAsync();
+                int newId = carts.Count + 1;
+
+                cartItems.Add(new CartItem { Dish = dish });
+
+                cart.CartId = newId;
+                cart.Items = cartItems;
+
+                HttpContext.Session.SetInt32("Cart", newId);
+
+                _context.Add(cart);
+                await _context.SaveChangesAsync();
+
+            }
+            else
+            {
+                var cartId = HttpContext.Session.GetInt32("Cart");
+                Cart cart = await _context.Carts.Include(x => x.Items).ThenInclude(z => z.Dish).SingleOrDefaultAsync(y => y.CartId == cartId);
+
+                var cartItem = await _context.CartItems.ToListAsync();
+                int newId = cartItem.Count + 1;
+
+                cart.Items.Add(new CartItem { CartItemId = newId, Dish = dish });
+
+                _context.Update(cart);
+                await _context.SaveChangesAsync();
+
+            }
+
+            return RedirectToAction("Index", "Dishes");
         }
 
         // GET: Cart/Create
